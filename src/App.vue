5ca2342bf4b99d58cb62ae4603e8ae68959089fc
Louis Eveillard <louis-ev@users.noreply.github.com>
2024-05-01 16:07:49 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+    <RouterView />
+  </div>
+</template>
+<script>
+import { RouterLink, RouterView } from 'vue-router'
+
+export default {
+  props: {},
+  components: {
+    RouterLink,
+    RouterView
+  },
+  data() {
+    return {}
+  },
+  created() {},
+  mounted() {},
+  beforeUnmount() {},
+  watch: {},
+  computed: {},
+  methods: {}
+}
+</script>
+<style lang="scss" scoped></style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>About</div>
+</template>
+<script>
+export default {
+  props: {},
+  components: {},
+  data() {
+    return {}
+  },
+  created() {},
+  mounted() {},
+  beforeUnmount() {},
+  watch: {},
+  computed: {},
+  methods: {}
+}
+</script>
+<style lang="scss" scoped></style>
