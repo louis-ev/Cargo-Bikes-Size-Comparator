6dc0b5b004b33f709ad69fd58353433301a1ad1a
Louis Eveillard <louis-ev@users.noreply.github.com>
2024-05-01 21:08:07 +0200

    wips

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
-  <div>
+  <div class="">
     <nav>
       <RouterLink to="/">Home</RouterLink>
       <RouterLink to="/about">About</RouterLink>
     </nav>
-    <RouterView />
+    <RouterView :cargo_data="cargo_data" />
   </div>
 </template>
 <script>
@@ -17,14 +17,53 @@ export default {
     RouterView
   },
   data() {
-    return {}
+    return {
+      cargo_data: undefined
+    }
   },
-  created() {},
+  created() {
+    this.loadCargo()
+  },
   mounted() {},
   beforeUnmount() {},
   watch: {},
   computed: {},
-  methods: {}
+  methods: {
+    loadCargo() {
+      fetch('/Cargo bike measurements - Bakfiets.csv')
+        .then((response) => response.text())
+        .then((csv) => {
+          const data = this.csvJSON(csv)
+
+          this.cargo_data = data.map((item) => {
+            item.Model = item.Model ? item.Model.replaceAll('/', '_') : 'NOT_AVAILABLE'
+            item.Manufacturer = item.Manufacturer
+              ? item.Manufacturer.replaceAll('/', '_')
+              : 'NOT_AVAILABLE'
+            item.id = item.Model + '/' + item.Manufacturer
+            return item
+          })
+        })
+        .catch((error) => console.error('Erreur lors du chargement du fichier CSV:', error))
+    },
+    csvJSON(csv) {
+      var lines = csv.split('\n')
+      var result = []
+      var headers = lines[0].split(',')
+
+      for (var i = 1; i < lines.length; i++) {
+        var obj = {}
+        var currentline = lines[i].split(',')
+
+        for (var j = 0; j < headers.length; j++) {
+          obj[headers[j]] = currentline[j]
+        }
+
+        result.push(obj)
+      }
+      return result
+    }
+  }
 }
 </script>
 <style lang="scss" scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,125 @@
 <template>
-  <div>Home</div>
+  <div class="_homeView">
+    <canvas ref="bikes" width="1280" height="720" />
+
+    {{ enabled_bikes }}
+
+    <label
+      class="_item"
+      v-for="item in cargo_data"
+      :key="item.id"
+      :for="item.id"
+      :data-active="enabled_bikes.includes(item.id)"
+    >
+      <input
+        type="checkbox"
+        :checked="enabled_bikes.includes(item.id)"
+        :id="item.id"
+        @change="toggleBike(item.id)"
+      />
+
+      <strong>{{ item.Manufacturer }}</strong> / {{ item.Model }}
+      {{ item['Overall length (cm)'] }}
+      <div v-if="findMatchingBike(item.id)">
+        <img :src="findMatchingBike(item.id).src" />
+      </div>
+    </label>
+  </div>
 </template>
 <script>
+import bikes from '@/assets/bike_images.json'
+
 export default {
-  props: {},
+  props: {
+    cargo_data: Array
+  },
   components: {},
   data() {
-    return {}
+    return {
+      enabled_bikes: ['Cargo/Chike', 'Muli/Muli Cycles']
+    }
   },
   created() {},
-  mounted() {},
+  mounted() {
+    this.showBikes()
+  },
   beforeUnmount() {},
-  watch: {},
+  watch: {
+    enabled_bikes: {
+      handler() {
+        this.showBikes()
+      },
+      deep: true
+    }
+  },
   computed: {},
-  methods: {}
+  methods: {
+    findMatchingBike(id) {
+      return bikes.find((i) => i.id === id)
+    },
+    toggleBike(id) {
+      if (this.enabled_bikes.includes(id)) {
+        this.enabled_bikes = this.enabled_bikes.filter((i) => i !== id)
+      } else {
+        this.enabled_bikes.push(id)
+      }
+    },
+    async showBikes() {
+      const canvas = this.$refs.bikes
+      const ctx = canvas.getContext('2d')
+      ctx.fillStyle = 'beige'
+      ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+      // get largest bike image
+      const max_bike_length = Math.max(
+        ...this.enabled_bikes.map((b) => {
+          const bike = this.findMatchingBike(b)
+          return bike?.bike_length_cm || 0
+        })
+      )
+
+      // factor to scale the image
+      const factor = canvas.width / max_bike_length
+
+      for await (const id of this.enabled_bikes) {
+        const bike_image = this.findMatchingBike(id)
+        if (!bike_image?.src) return
+        const img = new Image()
+        img.src = bike_image.src
+        await img.decode()
+
+        const img_ratio = img.width / img.height
+        const draw_w = bike_image.bike_length_cm * factor
+        const draw_h = draw_w / img_ratio
+
+        const left_margin = bike_image.left_margin_percent * draw_w
+        const top_margin = bike_image.bottom_margin_percent * draw_h
+
+        ctx.drawImage(img, -left_margin, 0, draw_w, draw_h)
+      }
+    }
+  }
 }
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+._homeView {
+  canvas {
+    width: 100%;
+    height: auto;
+  }
+}
+._item {
+  display: flex;
+  flex-direction: row nowrap;
+  align-items: center;
+  gap: 0.5rem;
+
+  &[data-active='true'] {
+    background-color: red;
+  }
+
+  img {
+    width: 100px;
+  }
+}
+</style>
