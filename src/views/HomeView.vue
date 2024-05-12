62b92b38e48e4c5904286d9ce0b67f89fd930745
Louis Eveillard <louis-ev@users.noreply.github.com>
2024-05-12 23:39:22 +0200

    LICENSE / added some more infos for AGPL

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -123,10 +123,36 @@
         </div>
         <div>
           Code/design
-          <a href="https://www.gnu.org/licenses/agpl-3.0.en.html" target="_blank"
-            >free, open-source under AGPL v3</a
-          >
+          <button type="button" class="noStyle" @click="show_license = !show_license">
+            free, open-source under AGPLv3</button
+          >, bike images from the manufacturer's website.
         </div>
+
+        <template v-if="show_license">
+          <div class="_license">
+            <p>
+              This website is free and open-source under AGPL v3. Bike images from the
+              manufacturer's website.
+            </p>
+            <pre>
+Copyright (C) 2024 Louis Eveillard
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see http://www.gnu.org/licenses/.
+              </pre
+            >
+          </div>
+        </template>
       </div>
     </div>
     <div class="_canvasWrapper">
@@ -166,6 +192,7 @@ export default {
       grid_step: 20,
 
       search_str: '',
+      show_license: false,
 
       canvas_composite_operation: 'source-over',
       globalCompositeOperations: [
@@ -204,7 +231,10 @@ export default {
   mounted() {
     this.showBikes()
     this.ro = new ResizeObserver(this.showBikes)
-    this.ro.observe(this.$el)
+    if (this.$el) {
+      debugger
+      this.ro.observe(this.$el)
+    }
   },
   beforeUnmount() {
     this.ro.unobserve(this.$el)
